9db8cf1569a9db418624f6f4e221fbea4fab41ca
Viloria01 <perezjulioernesto@gmail.com>
2020-06-11 03:28:37 -0300

    make it better

--- src/components/componentsNav/Toolbar/Toolbar.js
+++ src/components/componentsNav/Toolbar/Toolbar.js
@@ -14,9 +14,9 @@ const toolbar = props => (
         <div className="toolbar_navigation-items">
             <ul>
                 <li><a href="/">INICIO</a></li>
-                <li><a href="/">PSICOLOGOS</a></li>
-                <li><a href="/">SOBRE NOSOTROS</a></li>
-                <li><a href="/">CONTÁCTANOS</a></li>
+                <li><a href="/psicologos">PSICOLOGOS</a></li>
+                <li><a href="/nosotros">SOBRE NOSOTROS</a></li>
+                <li><a href="/contacto">CONTÁCTANOS</a></li>
             </ul>
         </div>
     </nav>
